fc81aa41018a92c214bba7d575045938d73c4f4d
Jiekang Hu <54252549+huj13k4n9@users.noreply.github.com>
2024-05-28 16:57:05 +0800

    change official kubernetes to isulad-specific version

--- init-k8s-isulad.sh
+++ init-k8s-isulad.sh
@@ -31,10 +31,8 @@ ARCH="amd64"
 CNI_PLUGINS_VERSION=$(get_latest_version "containernetworking/plugins")
 # CRICTL_VERSION="v1.28.0"
 CRICTL_VERSION=$(get_latest_version "kubernetes-sigs/cri-tools")
-# KUBE_RELEASE_VERSION="v0.16.2"
-KUBE_RELEASE_VERSION=$(get_latest_version "kubernetes/release")
 # K8S_RELEASE_VERSION="v1.29.3"
-K8S_RELEASE_VERSION="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
+K8S_RELEASE_VERSION="1.29.1-4"
 CNI_DIR="/opt/cni/bin"
 CNI_CONF_DIR="/etc/cni/net.d"
 BIN_DIR="/usr/local/bin"
@@ -42,7 +40,6 @@ POD_NET_CIDR="10.0.0.0/16"
 
 INFO "[*] CNI_PLUGINS_VERSION $CNI_PLUGINS_VERSION"
 INFO "[*] CRICTL_VERSION $CRICTL_VERSION"
-INFO "[*] KUBE_RELEASE_VERSION $KUBE_RELEASE_VERSION"
 INFO "[*] K8S_RELEASE_VERSION $K8S_RELEASE_VERSION"
 
 read
@@ -152,14 +149,14 @@ cp data/daemon.json /etc/isulad/daemon.json
 systemctl restart isulad
 DONE
 
-INFO "[*] Install kubectl"
-curl -LO "https://dl.k8s.io/release/${K8S_RELEASE_VERSION}/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/${K8S_RELEASE_VERSION}/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --client
-rm -f kubectl kubectl.sha256
-DONE
+# INFO "[*] Install kubectl"
+# curl -LO "https://dl.k8s.io/release/${K8S_RELEASE_VERSION}/bin/linux/amd64/kubectl"
+# curl -LO "https://dl.k8s.io/${K8S_RELEASE_VERSION}/bin/linux/amd64/kubectl.sha256"
+# echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+# install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# kubectl version --client
+# rm -f kubectl kubectl.sha256
+# DONE
 
 INFO "[*] Install CNI plugin"
 mkdir -p "$CNI_DIR"
@@ -171,17 +168,29 @@ mkdir -p "$BIN_DIR"
 curl -L "https://gh.con.sh/https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | tar -C $BIN_DIR -xz
 DONE
 
-INFO "[*] Install kubeadm and kubelet"
-cd $BIN_DIR
-curl -L --remote-name-all https://dl.k8s.io/release/${K8S_RELEASE_VERSION}/bin/linux/${ARCH}/{kubeadm,kubelet}
-chmod +x {kubeadm,kubelet}
+# INFO "[*] Install kubeadm and kubelet"
+# cd $BIN_DIR
+# curl -L --remote-name-all https://dl.k8s.io/release/${K8S_RELEASE_VERSION}/bin/linux/${ARCH}/{kubeadm,kubelet}
+# chmod +x {kubeadm,kubelet}
 
-curl -sSL "https://raw.gitmirror.com/kubernetes/release/${KUBE_RELEASE_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" | sed "s:/usr/bin:${BIN_DIR}:g" | tee /usr/lib/systemd/system/kubelet.service
-mkdir -p /usr/lib/systemd/system/kubelet.service.d
-curl -sSL "https://raw.gitmirror.com/kubernetes/release/${KUBE_RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${BIN_DIR}:g" | tee /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
+# curl -sSL "https://raw.gitmirror.com/kubernetes/release/${KUBE_RELEASE_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" | sed "s:/usr/bin:${BIN_DIR}:g" | tee /usr/lib/systemd/system/kubelet.service
+# mkdir -p /usr/lib/systemd/system/kubelet.service.d
+# curl -sSL "https://raw.gitmirror.com/kubernetes/release/${KUBE_RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${BIN_DIR}:g" | tee /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
+# systemctl enable --now kubelet
+# DONE
+
+INFO "[*] Install kubernetes packages"
+rpm -Uvh data/kubernetes-$K8S_RELEASE_VERSION.x86_64.rpm
+rpm -Uvh data/kubernetes-client-$K8S_RELEASE_VERSION.x86_64.rpm
+rpm -Uvh data/kubernetes-help-$K8S_RELEASE_VERSION.x86_64.rpm
+rpm -Uvh data/kubernetes-kubeadm-$K8S_RELEASE_VERSION.x86_64.rpm
+rpm -Uvh data/kubernetes-kubelet-$K8S_RELEASE_VERSION.x86_64.rpm
+rpm -Uvh data/kubernetes-master-$K8S_RELEASE_VERSION.x86_64.rpm
+rpm -Uvh data/kubernetes-node-$K8S_RELEASE_VERSION.x86_64.rpm
 systemctl enable --now kubelet
 DONE
 
+# Reference: https://github.com/cilium/cilium/issues/22933
 INFO "[*] Change permissions of /opt/cni"
 chown -R $(id -n -u):$(id -n -g) /opt/cni
 DONE
